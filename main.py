208e3da79bddc135e4947762385417886ed4e9cd
SeeXWH <mihaandre1@yamdex.ru>
2024-02-17 15:17:06 +0300

    qwe

--- main.py
+++ main.py
@@ -6,37 +6,55 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
-
-
 bot = Bot(token="6984947559:AAHpR2pdf9oEOwJ2ReS7yp4QUb5gNk91rO8")
 # Диспетчер
 dp = Dispatcher()
 
-flag = 0
 
 class UserState(StatesGroup):
+    newUser = State()
+    ageUser = State()
+    admin = State()
     name = State()
     addres = State()
 
-@dp.message(StateFilter(None), Command('test'))
+
+flag = 'newUse'
+
+
+@dp.message(Command('start'))
+async def user_start(message: types.Message, state: FSMContext):
+    await message.answer('Здравствуйте, введите свой номер телефона')
+    asyncio.run(central(flag))
+
+
+async def central(flag, state: FSMContext):
+    if flag == 'newUser':
+        asyncio.run(user_register())
+    else:
+        asyncio.run(test())
+
+
+@dp.message(StateFilter(None))
+async def test(message: types.Message, state: FSMContext):
+    await message.answer("Введите qweq")
+
+@dp.message(StateFilter(None))
 async def user_register(message: types.Message, state: FSMContext):
     await message.answer("Введите своё имя")
-    if flag == 1:
-        await state.set_state(UserState.name)
-    else:
-        await state.set_state(UserState.addres)
+    await state.set_state(UserState.name)
+
+
 @dp.message(StateFilter(UserState.name))
 async def user_register(message: types.Message, state: FSMContext):
-    #await state.update_data(username=message.text)
     await message.answer("Введите свою фамилию")
     await state.set_state(UserState.addres)
+
 
 @dp.message(StateFilter(UserState.addres))
 async def user_reg(message: types.Message, state: FSMContext):
     await message.reply('Иди нахуй')
     await state.clear()
-
-
 
 
 async def main():
