794200da8a3562194573b179862ca1bb0e3a6735
SlugPlay <SlugPlay@yandex.ru>
2024-02-17 15:16:23 +0300

    first

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import json
+
 from aiogram import Bot, Dispatcher, types, fsm
 from aiogram.fsm import state
 from aiogram.filters import Command, StateFilter
@@ -7,8 +9,11 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 
 
+#Зависимости----------------------------------------------------------------------------------------------------------
+file = open('bot_token.json', 'r')
+data = json.load(file).get('token')
+bot = Bot(token=str(data))
 
-bot = Bot(token="6984947559:AAHpR2pdf9oEOwJ2ReS7yp4QUb5gNk91rO8")
 # Диспетчер
 dp = Dispatcher()
 
@@ -16,17 +21,6 @@ dp = Dispatcher()
 class UserState(StatesGroup):
     name = State()
     addres = State()
-
-@dp.message(StateFilter(None), Command('test'))
-async def user_register(message: types.Message, state: FSMContext):
-    await message.answer("Введите своё имя")
-    await state.set_state(UserState.name)
-@dp.message(StateFilter(UserState.name))
-async def user_register(message: types.Message, state: FSMContext):
-    await state.update_data(username=message.text)
-    await message.answer("Введите свою фамилию")
-
-
 
 async def main():
     await dp.start_polling(bot)
