e1fa2d4b37d40e36585ce2b2ad8c4fe3fa95ec58
SlugPlay <SlugPlay@yandex.ru>
2024-02-20 16:24:17 +0300

    AAA

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+async def create_db():
+    global conn, cur
+
+    conn = sqlite3.connect('users_data.sql')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, phone_number TEXT, status TEXT, first_name TEXT, last_name TEXT, age TEXT, region TEXT, size TEXT, photo_front TEXT, photo_back TEXT, photo_profile TEXT)")
+    conn.commit()
+
+async def get_phone_status():
+    conn = sqlite3.connect('users_data.sql')
+    cur = conn.cursor()
+    result = cur.execute("SELECT phone_number, status FROM users").fetchall()
+    conn.commit()
+    return result
+
+
+async def create_profile(user_id, phone_number, status):
+    conn = sqlite3.connect('users_data.sql')
+    cur = conn.cursor()
+    user = cur.execute("SELECT 1 FROM users WHERE user_id == '{key}'".format(key=user_id)).fetchone()
+    if not user:
+        cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (user_id, phone_number, status, '', '', '', '', '', '', '', ''))
+        conn.commit()
+
+
+async def edit_profile(user_id, status, first_name, last_name, age, region, size, photo_front, photo_back, photo_profile):
+    cur.execute("UPDATE users SET status = '{}', first_name = '{}' last_name = '{}', age = '{}' region = '{}', size = '{}' photo_front = '{}', photo_back = '{}' photo_profile = '{}' WHERE user_id == '{}'".format(status, first_name, last_name, age, region, size, photo_front, photo_back, photo_profile, user_id))
+    conn.commit()
+
+
+def get_users(phone_number):
+    conn = sqlite3.connect('users_data.sql')
+    cur = conn.cursor()
+    result = cur.execute("SELECT first_name, last_name, age, region, size, photo_front, photo_back, photo_profile FROM users WHERE phone_number == '{key}'".format(key=phone_number)).fetchone()
+    return result
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import json
+import db
 
 from aiogram import Bot, Dispatcher, types, fsm, filters, F
 from aiogram.fsm import state
@@ -12,6 +13,9 @@ from aiogram.fsm.context import FSMContext
 file = open('bot_token.json', 'r')
 data = json.load(file).get('token')
 bot = Bot(token=str(data))
+user_info = []
+flag1 = ''
+
 
 # Диспетчер
 dp = Dispatcher()
@@ -21,7 +25,6 @@ recInformation = ''
 class UserState(StatesGroup):
     centr = State()
     newUser = State()
-    ageUser = State()
     admin = State()
 
 
@@ -32,26 +35,34 @@ class UserReg(StatesGroup):
     region = State()
     clothingSize = State()
 
-
-flag1 = 'newUser'
+class CoolerUser(StatesGroup):
+    ageUser = State()
 
 
 @dp.message(Command('start'))
 async def user_start(message: types.Message, state: FSMContext):
+    await db.create_db()
     await message.answer('Здравствуйте, введите свой номер телефона')
     await state.set_state(UserState.centr)
 
 
 @dp.message(StateFilter(UserState.centr))
 async def user_start(message: types.Message, state: FSMContext):
-    flag1 = str(message.text)
-    # db_func(flag1)
+    phone = str(message.text)
+    data_users = await db.get_phone_status()
+    flag1 = 'newUser'
+    for i in range(len(data_users)):
+        if phone == str(data_users[i][0]):
+            flag1 = str(data_users[i][1])
+            print(flag1)
     if flag1 == 'newUser':
         await message.answer('Введите свое имя')
         await state.set_state(UserState.newUser)
     elif flag1 == 'ageUser':
-        await state.set_state(UserState.ageUser)
+        print(1)
+        await state.set_state(UserState.newUser)
     elif flag1 == 'admin':
+        print(3)
         await state.set_state(UserState.admin)
 
 
@@ -66,23 +77,27 @@ async def user_start(message: types.Message, state: FSMContext):
 
 @dp.message(StateFilter(UserState.newUser))
 async def reg(message: types.Message, state: FSMContext):
+    recInformation = str(message.text)
+    user_info.append(recInformation)
     await message.answer('Введите свою фамилию')
     await state.set_state(UserReg.lastName)
-    recInformation = ''
+    
 
 
 @dp.message(StateFilter(UserReg.lastName))
 async def reg(message: types.Message, state: FSMContext):
+    recInformation = str(message.text)
+    user_info.append(recInformation)
     await message.answer('Введите свой возраст')
     await state.set_state(UserReg.age)
-    recInformation = ''
 
 
 @dp.message(StateFilter(UserReg.age))
 async def reg(message: types.Message, state: FSMContext):
+    recInformation = str(message.text)
+    user_info.append(recInformation)
     await message.answer('Выберите свой регион')
     await state.set_state(UserReg.region)
-    recInformation = ''
 
 
 # @dp.message(Command('photo'))
@@ -90,6 +105,17 @@ async def reg(message: types.Message, state: FSMContext):
 #     await message.answer('пришлите фото')
 #     file = await bot.get_file(message.document.file_id)
 #     await message.photo(file)
+
+@dp.message(StateFilter(CoolerUser.ageUser))
+async def menu(message: types.Message, state: FSMContext):
+    print(2)
+    await message.answer('Олух')
+
+
+@dp.message(StateFilter(UserState.admin))
+async def menu(message: types.Message, state: FSMContext):
+    print(4)
+    await message.answer('Тварь')
 
 
 @dp.message()
